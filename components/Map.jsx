180f0df9a1189f854cac21fd18164a6dd87a2566
washedlih <khanglee11@gmail.com>
2023-02-02 13:27:14 -0500

    Updated zoom level

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,8 +3,8 @@ import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 const Map = ({ data }) => {
   const center = useMemo(
     () => ({
-      lat: data.lat ? data.lat : 42.428,
-      lng: data.lon ? data.lon : -71.0618,
+      lat: data.lat ? data.lat : 37.7768,
+      lng: data.lon ? data.lon : -122.416,
     }),
     [data]
   );
@@ -12,7 +12,7 @@ const Map = ({ data }) => {
     <div className="h-full w-full">
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
         <GoogleMap
-          zoom={12}
+          zoom={13}
           center={center}
           mapContainerStyle={{
             height: "100%",
